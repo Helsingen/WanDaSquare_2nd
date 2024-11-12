2eef04b1a7bb9decadbf51ab01e71c7612dffa36
Helsingen <1660961318@qq.com>
2024-11-12 21:35:13 +0800

    初始化工程

---
+++ Assets/script/基础属性.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class 人类基础属性 : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public int Level;
+    public double HP;
+    public double ATK;
+    public double CRT;
+    public double DEF;
+    public double MDEF;
+    public double Speed;
+    public double ARGE; //仇恨范围
+    public double FWSP; //前摇
+    public double BWSP; //后摇
+    public double bulletSpeed; //弹道速度
+    public double attackRange; //攻击范围
+    public int cost;
+    //技能组
+    
+    
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
